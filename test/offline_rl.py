74c59e96d05d4b5673719f773846509f99ad3651
Nicolas Perrin-Gilbert <perrin.isir@gmail.com>
2025-09-03 17:13:22 +0200

    back on IQL

--- test/offline_rl.py
+++ test/offline_rl.py
@@ -12,10 +12,25 @@ conf_dict.log_interval = 50_000
 conf_dict.save_train_state_interval = 50_000
 conf_dict.eval_episodes = 10
 
-algo = FQL(conf_dict)
-# env_name = "mujoco/halfcheetah/medium-v0"
+# algo = FQL(conf_dict)
+algo = IQL(conf_dict)
+env_name = "mujoco/halfcheetah/medium-v0"
 # env_name = "D4RL/antmaze/umaze-v1"
-env_name = "cube-single-play-singletask-v0"
+# env_name = "cube-single-play-singletask-v0"
+# (
+#     dataset, 
+#     env,
+#     env_creator,
+#     reward_normalizing_factor, 
+#     obs_mean, 
+#     obs_std, 
+#     ref_max_score
+#     ) = get_ogbench(
+#         env_name, 
+#         normalize_reward=False, 
+#         normalize_obs=True)
+# __import__("IPython").embed()
+
 (
     dataset, 
     env, 
@@ -24,23 +39,9 @@ env_name = "cube-single-play-singletask-v0"
     obs_mean, 
     obs_std, 
     ref_max_score
-    ) = get_ogbench(
+    ) = get_minari(
         env_name, 
         normalize_reward=False, 
         normalize_obs=True)
-__import__("IPython").embed()
-
-# (
-#     dataset, 
-#     env, 
-#     env_creator,
-#     reward_normalizing_factor, 
-#     obs_mean, 
-#     obs_std, 
-#     ref_max_score
-#     ) = get_minari(
-#         env_name, 
-#         normalize_reward=False, 
-#         normalize_obs=True)
 train_offline(
     dataset, env_name, env, env_creator, algo, obs_mean, obs_std, ref_max_score)
